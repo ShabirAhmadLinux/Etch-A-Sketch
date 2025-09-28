7f9841fad96932bd40afc7a7a1a4659cb477491e
Shabir Ahmad <shabirahmadubuntu@gmail.com>
2025-09-28 13:27:54 +0000

    Create Grid of divs -Add classes to each divs(rows,cells) -Add a button to change square grid size -Add background-color to cell when hovered

---
+++ script.js
@@ -0,0 +1,27 @@
+function createGrid(n=16){
+    for(let i=0;i<n;i++){
+        let rowdiv=document.createElement("div");
+        rowdiv.classList.add("rowdiv")
+        sketchPad.appendChild(rowdiv);
+        for(let j=0;j<n;j++){
+            let cellBlock=document.createElement("div");
+            cellBlock.classList.add("cell");
+            rowdiv.appendChild(cellBlock);
+        }
+    }
+}
+let sketchPad=document.querySelector(".container");
+let gridSizeButton=document.querySelector(".gridSizeButton")
+createGrid();
+gridSizeButton.addEventListener('click',e=>{
+  let num=parseInt(prompt("Enter number(0 to 100) for number x number grid"));  
+  if(num>-1&&num<101){
+    while(sketchPad.hasChildNodes()){
+    sketchPad.removeChild(sketchPad.lastChild);
+  }
+    createGrid(num);
+  }
+  else{
+    alert("Dude it should be a number between 0 to 100");
+  }
+});
