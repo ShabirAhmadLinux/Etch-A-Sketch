4599c0b5a876a8f6e31efa56387aae7d4520d8a4
Shabir Ahmad <shabirahmadubuntu@gmail.com>
2025-10-15 17:01:29 +0000

    Add Option Buttons -Create button for   -colorfulMode(random colors each time)   -defaultMode(return to the default color)   -shadingMode
    Result: The former 2 options' logic were
    implemented. shadingMode yet to be done

--- script.js
+++ script.js
@@ -1,42 +1,79 @@
-function createGrid(n=16){
-    for(let i=0;i<n;i++){
-        let rowdiv=document.createElement("div");
-        rowdiv.classList.add("rowdiv")
-        sketchPad.appendChild(rowdiv);
-        for(let j=0;j<n;j++){
-            let cellBlock=document.createElement("div");
-            cellBlock.classList.add("cell");
-            rowdiv.appendChild(cellBlock);
-        }
+function createGrid(n = 16) {
+  for (let i = 0; i < n; i++) {
+    let rowdiv = document.createElement("div");
+    rowdiv.classList.add("rowdiv")
+    sketchPad.appendChild(rowdiv);
+    for (let j = 0; j < n; j++) {
+      let cellBlock = document.createElement("div");
+      cellBlock.classList.add("cell");
+      rowdiv.appendChild(cellBlock);
     }
-}
-let sketchPad=document.querySelector(".container");
-let gridSizeButton=document.querySelector(".gridSizeButton")
-createGrid();
-gridSizeButton.addEventListener('click',e=>{
-  let num=parseInt(prompt("Enter number(0 to 100) for number x number grid"));  
-  if(num>-1&&num<101){
-    while(sketchPad.hasChildNodes()){
-    sketchPad.removeChild(sketchPad.lastChild);
   }
+}
+
+let sketchPad = document.querySelector(".sketchpad");
+let gridSizeButton = document.querySelector(".gridSizeButton")
+createGrid();
+gridSizeButton.addEventListener('click', e => {
+  let num = parseInt(prompt("Enter number(0 to 100) for number x number grid"));
+  if (num > -1 && num < 101) {
+    while (sketchPad.hasChildNodes()) {
+      sketchPad.removeChild(sketchPad.lastChild);
+    }
     createGrid(num);
   }
-  else{
+  else {
     alert("Dude it should be a number between 0 to 100");
   }
 });
 
-    function changeBackground(e){
-     
-        if(e.target.className==="cell"){
-          e.target.style.backgroundColor="green";
-        }
+
+function changeBackground(e) {
+
+  if (e.target.className === "cell") {
+    e.target.style.backgroundColor = "green";
+  }
+}
+
+function random() {
+  return Math.floor(Math.random() * 256);
+}
+
+let defaultButton = document.querySelector(".defaultModeButton");
+defaultButton.addEventListener("click", () => {
+  changeBackground = function (e) {
+
+    if (e.target.className === "cell") {
+      e.target.style.backgroundColor = "green";
     }
-    sketchPad.addEventListener("mousedown",()=>{
-      sketchPad.addEventListener("mouseover",changeBackground);
-      sketchPad.addEventListener("mouseup",()=>{
-        sketchPad.removeEventListener("mouseover",changeBackground);
-      });
-    });
-  
-     
+  }
+});
+
+
+let colorfulButton = document.querySelector(".colorfulModeButton");
+colorfulButton.addEventListener("click", () => {
+  changeBackground = function (e) {
+    if (e.target.className === "cell") {
+      let red = random();
+      let green = random();
+      let blue = random();
+
+      e.target.style.backgroundColor = `rgb(${red} ${green} ${blue})`;
+    }
+  }
+});
+
+
+
+
+
+sketchPad.addEventListener("mousedown", (e) => {
+  changeBackground(e);
+  sketchPad.addEventListener("mouseover", changeBackground);
+  sketchPad.addEventListener("mouseup", () => {
+    sketchPad.removeEventListener("mouseover", changeBackground);
+    sketchPad.removeEventListener("mouseup", changeBackground);
+  });
+});
+
+
